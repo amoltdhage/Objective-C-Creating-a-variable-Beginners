4546ddd16cdd91d7d27473823a5a5d1b9064950c
Amol Dhage <amoltdhage@gmail.com>
2016-11-06 23:12:36 +0530

    Initial Commit

---
+++ ADCreatingAVariable/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ADCreatingAVariable
+//
+//  Created by Amol Tukaram Dhage on 06/11/16.
+//  Copyright © 2016 AmolTDhage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
